96595f208f8cb0f1c61b694f08658baca1750538
jeffqureshy <jqureshy@gmail.com>
2022-03-21 02:20:54 +0000

    fix dup sessions by removing created_at from int

--- greenery/models/marts/product/intermediate/int_user_sessions_agg.sql
+++ greenery/models/marts/product/intermediate/int_user_sessions_agg.sql
@@ -6,7 +6,6 @@ with fact_events as (
 )
 select session_id
     ,   user_id
-    ,   created_at
     {% for event_type in event_types %}
     ,   sum( case when event_type = '{{event_type}}' then 1 else 0 end ) as {{event_type}}_count
     {% endfor %}
@@ -14,6 +13,6 @@ from fact_events
 --where product_id is not null
 group by session_id
     ,   user_id
-    ,   created_at
+
 
 
